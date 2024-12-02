3be7fdcc7993c05d134975aa5a14fb75833907c9
ridamsinghal25 <ridamsinghal25@gmail.com>
2024-12-02 16:49:04 +0530

    add initial setup of project to codebase

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl bg-green-500 font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
